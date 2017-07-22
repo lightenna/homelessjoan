b6e6164f372385ed9baa818fddfd656c2cab8d82
cleverlight <github.account@cleveraspaint.com>
2017-07-22 09:12:12 -0400

    Moved everything over to app_dev for rapid iteration on live site; stubbed out filtering function

--- htdocs/src/AppBundle/Controller/DefaultController.php
+++ htdocs/src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\DependencyInjection\TwitterAPIExchange;
 
 class DefaultController extends Controller
 {
@@ -13,9 +14,49 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $mentions = $this->getTwitterActivity();
+        $user_blacklist = $this->findRemovedUsers($mentions);
+        $filtered_mentions = $this->filterMentionsByUser($user_blacklist, $mentions);
         // replace this example code with whatever you need
         return $this->render('AppBundle:front:index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
+            'mentions' => $filtered_mentions,
         ]);
     }
+
+    //
+    // Helpers
+    //
+
+    private function getTwitterActivity()
+    {
+        // hack settings in here for now
+        $settings = array(
+            'oauth_access_token' => "888708594635550721-iU9d9sJy7bY1s9IhAviyeeXxFAlK4Ia",
+            'oauth_access_token_secret' => "2jxx9U5NbLqDDqxiDOUH4fiywmnz1tXey7zL9jYdZVsVQ",
+            'consumer_key' => "8t5rHV5Js6SvEtcp0ViitjXcn",
+            'consumer_secret' => "PgvMAbv915NbLpDA7SjxANnkjhB46AMM7JclTr145Nyp7zztvh"
+        );
+        // first request for mentions
+        $url = 'https://api.twitter.com/1.1/statuses/mentions_timeline.json';
+        $requestMethod = 'GET';
+        $twitter = new TwitterAPIExchange($settings);
+        $mentions = $twitter->buildOauth($url, $requestMethod)
+            ->performRequest();
+        // parse response
+        $mentionsObject = json_decode($mentions);
+        return $mentionsObject;
+    }
+
+    private function findRemovedUsers($mentions)
+    {
+
+    }
+
+    private function filterMentionsByUser($user_blacklist, $mentions)
+    {
+
+    }
+
+
 }
